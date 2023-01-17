ecc922bdd0b0aa5047e7d9c29c3cb9c957e64922
Tiger Chen <tiger@visbot.com.cn>
2023-01-17 11:03:48 +0800

    add sitl_gazebo changes to sitl_gazebo.diff.

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 #roslaunch px4 mavros_posix_sitl.launch
 #roslaunch px4 posix_sitl.launch
 #roslaunch px4 multi_uav_mavros_sitl.launch
-#roslaunch px4 visbot_swarm.launch
+roslaunch px4 visbot_swarm.launch
 #roslaunch px4 multi_uav_mavros_sitl_sdf.launch
-roslaunch px4 visbot_sitl.launch
+#roslaunch px4 visbot_sitl.launch
 
