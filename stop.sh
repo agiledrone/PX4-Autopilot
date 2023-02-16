802c4d416e941d804a44c535bfd6e1559618fef0
Tiger Chen <tiger@visbot.com.cn>
2023-02-16 13:40:33 +0800

    move ./gazebo_sitl.tgz to gazebo_sitl_patch add stop.sh

---
+++ stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+killall -9  run.sh
+killall -9  roslaunch
+killall -9  px4
+killall -9  mavros_node
+killall -9  captain
+killall -9  ego_planner_node
+killall -9  geometric_controller_node
+killall -9  traj_server
+killall -9  gzserver
+killall -9  gzclient
+
