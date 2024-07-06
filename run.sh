2431b64f83a75373fc954d269b72e77c173f7868
agiledrone <agile.drone@gmail.com>
2024-07-06 15:45:05 +0800

    add many sim scenarios

--- run.sh
+++ run.sh
@@ -9,13 +9,21 @@ export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 #roslaunch px4 posix_sitl.launch
 #roslaunch px4 multi_uav_mavros_sitl.launch
 #roslaunch px4 multi_uav_mavros_sitl_sdf.launch
+
 #roslaunch px4 visbot.launch
+roslaunch px4 visbot_swarm.launch
 #roslaunch px4 visbot_swarm_owl.launch
 #roslaunch px4 visbot_swarm_owl-square.launch
+#roslaunch px4 visbot_swarm_maze.launch
+#roslaunch px4 visbot_swarm_cafe.launch
 
 #roslaunch px4 visbot-maze.launch
 #roslaunch px4 visbot-square.launch
-roslaunch px4 visbot-playground.launch
+#roslaunch px4 visbot-playground.launch
 #roslaunch px4 visbot-cafe.launch
 #roslaunch px4 visbot-pier.launch
-#roslaunch px4 visbot-field.launch
+
+#roslaunch px4 visbot_ext.launch
+
+
+
